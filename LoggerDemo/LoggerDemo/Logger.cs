44fef299473f145b5576f386cd788986af443dd5
Harshithaa123 <94697901+Harshithaa123@users.noreply.github.com>
2024-04-27 23:05:09 +0530

    Add files via upload

---
+++ LoggerDemo/LoggerDemo/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LoggerDemo
+{
+    public static class Logger
+    {
+        public static void WriteLogMessage(string message)
+        {
+            string logpath = ConfigurationManager.AppSettings["logPath"];
+            using(StreamWriter writer=new StreamWriter(logpath))
+            {
+                writer.WriteLine($"{DateTime.Now} : {message}");
+            }
+        }
+    }
+}
